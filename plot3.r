596ac53b8d6546214d5d2d4336d305421ba5432d
wrbuckin <Bill@Bills-MacBook-Pro.local>
2015-08-09 07:15:23 -0500

    updates to loading

--- plot3.r
+++ plot3.r
@@ -1,5 +1,18 @@
 #3
-source(loadData.r)
+filename <- "~/Downloads/household_power_consumption.txt"
+data <- read.table(filename,header = TRUE,sep = ";",colClasses = c("character", "character", rep("numeric",7)), na = "?")
+dim(data) 
+attach(data)
+
+subset <- Date == "1/2/2007" | Date == "2/2/2007"
+newData <- data[subset, ]
+attach(newData)
+x <- paste(Date, Time)
+newData$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+rownames(newData) <- 1:nrow(newData)
+dim(newData) 
+attach(newData)
+
 png(filename = "plot3.png", 
     width = 480, height = 480,
     units = "px", bg = "transparent")
